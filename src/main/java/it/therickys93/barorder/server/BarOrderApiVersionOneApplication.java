b923bda26b83f7c7ff009ff352a6e9c8ed167e0e
Riccardo Crippa <therickys93@gmail.com>
2016-12-28 16:04:12 +0100

    tolte alcune caratteristiche al server e lasciate solo quelle basi

--- src/main/java/it/therickys93/barorder/server/BarOrderApiVersionOneApplication.java
+++ src/main/java/it/therickys93/barorder/server/BarOrderApiVersionOneApplication.java
@@ -13,16 +13,20 @@ public class BarOrderApiVersionOneApplication extends Application {
 		Router router = new Router(getContext());
 		
 		// POST /v1/test
+		/*
 		router.attach(BarOrderInfo.testPath(), TestResource.class);
 		getLogger().info(BarOrderInfo.testPathInfo());
+		*/
 		
 		// POST /v1/insertOrder
 		router.attach(BarOrderInfo.insertOrderPath(), InsertOrder.class);
 		getLogger().info(BarOrderInfo.insertOrderPathInfo());
 		
 		// POST /v1/updateOrder
+		/*
 		router.attach(BarOrderInfo.updateOrderPath(), UpdateOrder.class);
 		getLogger().info(BarOrderInfo.updateOrderPathInfo());
+		*/
 		
 		// GET /v1/products
 		router.attach(BarOrderInfo.productsPath(), GetProducts.class);
@@ -33,12 +37,16 @@ public class BarOrderApiVersionOneApplication extends Application {
 		getLogger().info(BarOrderInfo.completeOrderPathInfo());
 		
 		// POST /v1/deleteOrder
+		/*
 		router.attach(BarOrderInfo.deleteOrderPath(), DeleteOrder.class);
 		getLogger().info(BarOrderInfo.deleteOrderPathInfo());
+		*/
 		
+		/*
 		// GET /v1/order/{id}
 		router.attach(BarOrderInfo.orderWithIdPath(), OrderWithId.class);
 		getLogger().info(BarOrderInfo.orderWithIdPathInfo());
+		*/
 		
 		// GET /v1/orders
 		router.attach(BarOrderInfo.ordersPath(), Orders.class);
