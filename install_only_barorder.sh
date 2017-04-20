0f29e7c9729348951a9ae3fab4ddf69d9e16c6bf
Riccardo Crippa <therickys93@gmail.com>
2017-04-20 16:49:36 +0200

    sistemato file installazione barorder

--- install_only_barorder.sh
+++ install_only_barorder.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# controlla che docker-compose sia installato
+apt-get update
+apt-get install python-pip
+pip install docker-compose
+
 curl -sL -o docker-compose.yml https://raw.githubusercontent.com/therickys93/barorder/master/docker-compose.production.yml
 docker-compose build
 docker-compose up -d
