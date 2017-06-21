d4bae19f8d1bf6906908a8cb993cf3d88378e18c
Riccardo Crippa <therickys93@gmail.com>
2017-06-21 12:20:25 +0200

    corretto piccolo bug

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-java -cp build/libs/barorder.jar it.therickys93.barorder.database.DatabaseInfo
-java -cp build/libs/barorder.jar it.therickys93.barorder.server.Server
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+java -cp $DIR/build/libs/barorder.jar it.therickys93.barorder.database.DatabaseInfo
+java -cp $DIR/build/libs/barorder.jar it.therickys93.barorder.server.Server
